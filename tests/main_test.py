e2f74aab36c92d93d31deecdfd4b5ebefd266de9
Manoji <mmanojipec@gmail.com>
2025-12-07 19:11:42 -0500

    test changes

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,21 @@
+# from pyspark.sql import SparkSession
+import pytest
+
+import sys
+sys.path.append("../src")
+
 from capstone_pipeline.main import get_taxis, get_spark
+
+@pytest.fixture
+def serverless():
+    try:
+        from databricks.connect import DatabricksSession
+
+        return DatabricksSession.builder.serverless(True).getOrCreate()
+    except ImportError:
+        return None
 
 
 def test_main():
     taxis = get_taxis(get_spark())
-    assert taxis.count() > 5
+    assert taxis.count() > 21931
